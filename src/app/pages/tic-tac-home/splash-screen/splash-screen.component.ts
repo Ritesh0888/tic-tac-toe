faf27fa51eaf14bc33b4e1f720246b45d86c7dba
shivam.jha1 <shivam.jha@pw.live>
2022-09-26 00:24:06 +0530

    added splash screen

--- src/app/pages/tic-tac-home/splash-screen/splash-screen.component.ts
+++ src/app/pages/tic-tac-home/splash-screen/splash-screen.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-splash-screen',
   templateUrl: './splash-screen.component.html',
-  styleUrls: ['./splash-screen.component.scss']
+  styleUrls: ['./splash-screen.component.scss'],
+  encapsulation: ViewEncapsulation.None,
 })
 export class SplashScreenComponent implements OnInit {
+  windowWidth: string = '';
+  showSplash: boolean = true;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.showSplashLoading();
   }
 
+  showSplashLoading() {
+    setTimeout(() => {
+      this.windowWidth = '-' + window.innerWidth + 'px';
+      setTimeout(() => {
+        this.showSplash = !this.showSplash;
+      }, 500);
+    }, 1500);
+  }
 }
